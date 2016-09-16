7201c474a3435de0f617930ca1cc1366a5024390
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2016-09-16 09:41:51 +0200

    Php Info.

---
+++ phpinfo.php
@@ -0,0 +1,20 @@
+<?php
+	/*
+	# 
+	# MODULE DESCRIPTION:
+	# phpinfo.php
+	# 
+	# 
+	# AUTHORS:
+	# Author Name		Raffaele Ficcadenti
+	# Author email		raffaele.ficcadenti@gmail.com
+	# 
+	# 
+	# HISTORY:
+	# -[Date]---    -[Who]-------------         -[What]---------------------------------------
+	# 16-09-2016    Ficcadenti Raffaele         phpinfo()
+	# --
+	#
+	*/
+	phpinfo();
+?>
