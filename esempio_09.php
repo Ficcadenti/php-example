5a09aab1ead964b5bcb5a0e0de82284e107ebef2
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2016-09-20 15:49:33 +0200

    Array

--- esempio_09.php
+++ esempio_09.php
@@ -29,17 +29,60 @@
 		    font-size: 30px;
 		    color: #0000FF;
 		}
+		
+		h2 {
+			margin-left: 30px;
+		}
+		ul#test {
+			 list-style: none;
+			 margin-left: 0;
+			 padding-left: 1em;
+			 text-indent: -1em;
+		}
+		ul#test li:before {
+ 			content: "\0BB \020";
+ 		}
+
+ 		ul#menu {
+		    font-family: Verdana, sans-serif;
+		    font-size: 12px;
+		    margin: 0;
+		    padding: 0;
+		    list-style: none;
+		}
+		ul#menu li {
+		    background-color: #00FF00;
+		    border-left: 5px solid #54BAE2;
+		    display: block;
+		    width: 150px;
+		    height: 30px;
+		    margin: 2px 0;
+		}
+		ul#menu li a {
+		    color: #fff;
+		    display: block;
+		    font-weight: bold;
+		    line-height: 30px;
+		    padding-left: 15px;
+		    text-decoration: none;
+		    width: 135px; /* 150px - 15px (padding) */
+		    height: 30px;
+		}
+		ul#menu li.active, ul#menu li:hover {
+		    background-color: #54BAE2;
+		    border-left: 5px solid #FF831C;
+		}
+
 	</style>
 </head>
 <body>
-	<b>
 	<!-- Codice PHP -->
 	<?php
-		include("my_lib.php");
+		include("./my_lib.php");
 
 		function printH($h,$str)
 		{
-			println("<$h>$str</$h>"); // funzione definita dall'utente
+			print("<$h>$str</$h>"); // funzione definita dall'utente
 		}
 
 		function capitolo($str) /* utilizzo di variabili statiche */
@@ -47,11 +90,38 @@
 			static $num_capitolo = 0;
 			$num_capitolo++;
 			printH("h1","$num_capitolo. $str");
+			return $num_capitolo;
 		}
 
+		function paragrafo($str,$cap) /* utilizzo di variabili statiche */
+		{
+			static $num_paragrafo = 0;
+			$num_paragrafo++;
+			printH("h2","$cap.$num_paragrafo. $str");
+		}
+
+		function elencoPuntato($elenco)
+		{
+			println("<ul  id=\"menu\">");
+			foreach ($elenco as $value) 
+			{
+				print("<li><a>$value</a></li>");
+			}
+			println("</ul>");
+		}
 	?>
 	<?php
-		capitolo("Array");
+		
+		$famiglia=array("Valeria","Raffaele","Sofia","Maria","Gabriele");
+		$famiglia[]="Francesco";
+
+		$functionHolder="capitolo";
+		$num_capitolo=$functionHolder("Array");
+		
+		$functionHolder="paragrafo";
+		$functionHolder("Famiglia",$num_capitolo);
+		$functionHolder("Componenti",$num_capitolo);
+
+		elencoPuntato($famiglia);
 	?>
-	</b>
 </body>
