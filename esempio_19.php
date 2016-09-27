145d9a043191c484a4f2aa08b33552bc2ce39a19
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2016-09-27 16:05:45 +0200

    Database DBM

--- esempio_19.php
+++ esempio_19.php
@@ -100,28 +100,66 @@
 			  array("date",     "D"),
 			  array("name",     "C",  50),
 			  array("age",      "N",   3, 0),
-			  array("email",    "C", 128),
-			  array("ismember", "L")
+			  array("email",    "C", 128)
 			);
 
 			// creation
 			if (!dbase_create($nome_db, $def)) 
 			{
-			  echo "Error, can't create the database\n";
+			  	echo "Error, can't create the database\n";
 			}
 
-			$dbh=dbase_open($nome_db,0)or die ("Impossibile aprire il db: $nome_db !!!");
+			$dbh=dbase_open($nome_db,2)or die ("Impossibile aprire il db: $nome_db !!!");
 
 			if ($dbh) 
 			{
-				$num_capitolo=capitolo("Database DBM")	  ;
+				$record=array();
+				$num_elementi=0;
+				$num_capitolo=capitolo("Database DBM");
+
+				print("<div id=\"m30\">");
+					array_push($record,array(date("ymd"),"Raffaele",40,"raffaele.ficcadenti@gmail.com"));
+					array_push($record,array(date("ymd"),"Valeria",40,"valeria5.greco@gmail.com"));
+					array_push($record,array(date("ymd"),"Francesco",22,"francesco.greco@gmail.com"));
+					array_push($record,array(date("ymd"),"Luca",39,"luca.ficcadenti@gmail.com"));
+					array_push($record,array(date("ymd"),"Roberto",46,"roberto.greco@gmail.com"));
+
+					foreach ($record as $key => $value) 
+					{
+							
+						if(dbase_add_record($dbh,$value))
+						{
+							println("Record inserito $value[1]");
+						}
+						else
+						{
+							println("ERROR: Eecord non inserito");
+						}
+					}
+
+					$tmp=array(date("ymd"),"Francesco",23,"francesco.greco@gmail.com");
+					dbase_replace_record($dbh,$tmp,3);
+
+					$record_numbers = dbase_numrecords($dbh);
+					for ($i = 1; $i <= $record_numbers; $i++) 
+					{
+						if($i%2==0) // cancello solo i pari
+						{
+							dbase_delete_record($dbh, $i);
+						}
+					}
+					dbase_pack($dbh);
+					
+				print("</div>");
 				dbase_close($dbh);
+
+				$num_capitolo=capitolo("Info");
 			}
 		?>
-		<a href="http://php.net/manual/en/ref.dbase.php" target="_blank">dBase Functions</a><br>
-		<a href="https://pecl.php.net/package/dbase/5.1.0/windows" target="_blank">dBase download</a><br>
-		<a href="http://dbfconv.com/" target="_blank">DBF Converter</a><br>
-		
-
+		<div id="m30">
+			<a href="http://php.net/manual/en/ref.dbase.php" target="_blank">dBase Functions</a><br>
+			<a href="https://pecl.php.net/package/dbase/5.1.0/windows" target="_blank">dBase download</a><br>
+			<a href="http://dbfconv.com/" target="_blank">DBF Converter</a><br>
+		</div>	
 	</body>
 </hmtl>
