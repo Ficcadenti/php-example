bcfd053fe01afea760fc4d0a0a96302fac415323
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2016-10-07 15:51:57 +0200

    Casting

---
+++ Lezione-14 - I dati nel dettaglio/esempio_32.php
@@ -0,0 +1,81 @@
+<!--
+	# 
+	# MODULE DESCRIPTION:
+	# esempio_32.html
+	# 
+	# 
+	# AUTHORS:
+	# Author Name		Raffaele Ficcadenti
+	# Author email		raffaele.ficcadenti@gmail.com
+	# 
+	# 
+	# HISTORY:
+	# -[Date]-      -[Who]-               -[What]-
+	# 06-10-2016    Ficcadenti Raffaele         
+	# -
+	#
+-->
+<?php
+	date_default_timezone_set('UTC');
+	print("<br>");
+
+	if( (include("../assets/lib/my_lib.php")) == 'success' ) 
+	{
+		println("Include \"my_lib.php\" ok. !!!!");
+		println();
+	}
+	else
+	{
+		include("../assets/lib/my_lib_test.php");
+	}
+
+	function printH($h,$str)
+	{
+		print("<$h>$str</$h>"); // funzione definita dall'utente
+	}
+
+	function capitolo($str) /* utilizzo di variabili statiche */
+	{
+		static $num_capitolo = 0;
+		$num_capitolo++;
+		printH("h1","$num_capitolo. $str");
+		return $num_capitolo;
+	}
+
+	function paragrafo($str,$cap) /* utilizzo di variabili statiche */
+	{
+		static $num_paragrafo = 0;
+		$num_paragrafo++;
+		printH("h2","$cap.$num_paragrafo. $str");
+	}
+
+	function stampaErrorre($type,$str)
+	{
+		echo "<font color=\"red\">$type : $str </font><br>";
+	}
+	function str_bool($val)
+	{
+		$str="false";
+		if($val>0) $str="true";
+		return $str;
+	}
+
+	println("<strong>Codice sorgente: </strong>".$_SERVER["PHP_SELF"]);
+	println();
+
+?>
+
+<hmtl>
+	<head>
+		<title>sorgente: esempio_32.html</title>
+		<!-- Sezione per i CSS -->
+		<!-- load default.css -->
+		<?php
+			include ("../assets/css/default.css");
+		?>
+	</head>
+	<body>
+		<?php
+		?>
+	</body>
+</hmtl>
--- README.md
+++ README.md
@@ -163,4 +163,11 @@ Raffaele.
 			<br>
 		</ul>
 	</li>
+	<li><strong>LEZIONE N. 12</strong> - Tipi di dati, nel dettaglio.
+		<ul>
+			<br>
+			<li><b>esempio_32.php</b>: Il casting.</li>
+			<br>
+		</ul>
+	</li>
 </ul>
