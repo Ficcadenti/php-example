70a5c49b08ceaf526b41a6a0b73c268e186849ae
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2016-11-11 14:08:35 +0100

    JSON Validator

--- Lezione-22 - PHP e JSON/esempio_54.php
+++ Lezione-22 - PHP e JSON/esempio_54.php
@@ -149,6 +149,7 @@
 
 		<img src="./pleasewait.gif" alt="pleasewait" height="13" width="280"><br> 
 		<a href="http://www.json.org/json-it.html" target="_blank">JSON</a><br>
+		<a href="http://jsonlint.com/" target="_blank">The JSON Validator </a><br>
 		<a href="http://php.net/manual/en/book.json.php" target="_blank">PHP json</a><br>
 		<a href="http://www.w3schools.com/php/" target="_blank">w3schools<span class="dotcom">.com</span></a><br>
 	</body>
